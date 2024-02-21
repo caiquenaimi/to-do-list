400ea1e167883c91fc5e8f76d1986b078462c4fe
caiquenaimi <caiquenaimi.564@gmail.com>
2024-02-21 09:26:18 -0300

    Adiciona campo de entrada e botão para adicionar tarefa na lista

--- App.jsx
+++ App.jsx
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [task, setTask] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>To Do List✍</Text>
+      <View style={styles.box}>
+        <TextInput
+          placeholder="Digite uma nova tarefa"
+          style={styles.input}
+          onChangeText={setTask}
+          value={task}
+        />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.txt}>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +31,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding : 50,
   },
+  box: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  input: {
+    border : "1px solid #000",
+    padding: 10,
+    width: 200,
+  },
+  btn: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+  },
+  txt: {
+    color: "#fff",
+  },
+  
 });
